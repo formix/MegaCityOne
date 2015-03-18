e58f13ee6f18cbcdfa37df250cd3d34d12180ac1
formix <jeanphilippe.gravel@gmail.com>
2015-03-18 08:09:27 -0400

    Going forward in MVC authentication

---
+++ MegaCityOne/MVC/AuthenticateAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace MegaCityOne.Mvc
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthenticateAttribute : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            var user = (UserData)filterContext.HttpContext.Session["User"];
+            if (user != null)
+            {
+                filterContext.HttpContext.User = new GenericPrincipal(
+                    new GenericIdentity(user.Name), user.Roles);
+            }
+
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (AllowAnonymous(filterContext))
+            {
+                return;
+            }
+
+            var user = filterContext.HttpContext.User;
+            if (user == null || !user.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+
+
+        private bool AllowAnonymous(AuthenticationChallengeContext filterContext)
+        {
+            return filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true) ||
+                filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true);
+        }
+    }
+}
